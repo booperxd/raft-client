f8d720f8eba291bd4204c24da813acef0850303c
booperxd <coopervohs@gmail.com>
2021-12-16 16:21:25 -0500

    inital start to client code

---
+++ raftClient/src/main/java/lvc/cds/raftClient/RaftStub.java
@@ -0,0 +1,70 @@
+package lvc.cds.raftClient;
+
+import java.util.concurrent.TimeUnit;
+
+import com.google.protobuf.Empty;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import lvc.cds.raft.proto.ClientMessage;
+import lvc.cds.raft.proto.RaftRPCGrpc;
+import lvc.cds.raft.proto.RaftRPCGrpc.RaftRPCStub;
+
+public class RaftStub {
+    String peer;
+    int port;
+
+    ManagedChannel channel;
+    RaftRPCStub stub;
+
+    RaftStub(String peer, int port) {
+        this.peer = peer;
+        this.port = port;
+        this.channel = null;
+        this.stub = null;
+    }
+
+    void connect() {
+        channel = ManagedChannelBuilder.forAddress(peer, port).usePlaintext().build();
+        stub = RaftRPCGrpc.newStub(channel);
+    }
+
+    RaftRPCStub getStub() {
+        return stub;
+    }
+
+    void shutdown() throws InterruptedException {
+        channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    void sendClientMessage(String log) {
+        ClientMessage request = ClientMessage.newBuilder()
+        .setLog(log)
+        .build();
+
+        getStub().clientRequest(request, new StreamObserver<Empty>() {
+            @Override
+            public void onNext(Empty value) {
+                // we have the peer string (IP address) available here.
+                System.out.println("received response from raft leader");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("Error contacting raft leader");
+            }
+
+            @Override
+            public void onCompleted() {
+                System.err.println("stream observer onCompleted");
+            }
+        });
+
+
+    }
+
+    void sendRequestVote() {
+
+    }
+}
